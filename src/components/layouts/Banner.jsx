b3846bf77b4b8f1cba6621b3ab15bf45f3371ffa
HabibaBinteAmir <raiyanbinamir25@gmail.com>
2024-11-30 16:23:06 +0600

    first commit

---
+++ src/components/layouts/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className="bg-bannerbg py-[300px] bg-cover bg-center bg-no-repeat shadow-lg">
+        
+
+    </div>
+  )
+}
+
+export default Banner
